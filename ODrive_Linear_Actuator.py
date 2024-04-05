262f3fe88aac43accbcb48d8cc9e01b583975d23
Lemon2311 <marcotighiliu@yahoo.com>
2024-04-06 01:07:53 +0300

    initialized repo, with basic code for linear actuator

---
+++ ODrive_Linear_Actuator.py
@@ -0,0 +1,65 @@
+import odrive
+from odrive.enums import AxisState, ControlMode
+import time
+
+def init(odrive):
+    odrive.axis0.requested_state = AxisState.CLOSED_LOOP_CONTROL
+    return odrive
+
+def setup_velocity_control(odrive):
+    odrive.axis0.controller.config.control_mode = ControlMode.VELOCITY_CONTROL
+    return odrive
+
+def check_position_delta(odrive, position_delta_margin, delta_time):
+    initial_position = odrive.axis0.pos_estimate
+    time.sleep(delta_time)
+    return abs(odrive.axis0.pos_estimate - initial_position) < position_delta_margin
+
+bounds = {}
+
+def bounds_detection_routine(odrive, delta_time=1):
+    
+    bound_left = None
+    bound_right = None
+
+    odrive.axis0.controller.input_vel = 1
+
+    while True:
+
+        print(f"pos:{odrive.axis0.pos_estimate};")
+
+        if(bound_right is None and check_position_delta(odrive, 0.1, delta_time)):
+            odrive.axis0.controller.input_vel = 0
+            bound_right = odrive.axis0.pos_estimate
+            odrive.axis0.controller.input_vel = -1
+
+        if(bound_right is not None and check_position_delta(odrive, 0.1, delta_time)):
+            odrive.axis0.controller.input_vel = 0
+            bound_left = odrive.axis0.pos_estimate
+            print(f"bound_left: {bound_left}; bound_right: {bound_right}")
+            bounds[odrive.serial_number] = (bound_left, bound_right)
+            return odrive
+
+def setup_position_control(odrive):
+    odrive.axis0.controller.config.control_mode = ControlMode.POSITION_CONTROL
+    return odrive
+
+def move_in_procents(odrive, procent_from_start, await_position_reach=True, position_delta_margin=0.1):
+    bound_left, bound_right = bounds[odrive.serial_number]
+    commanded_position = bound_left + (bound_right - bound_left) * procent_from_start / 100
+    odrive.axis0.controller.input_pos = commanded_position
+    
+    while await_position_reach:
+        current_position = odrive.axis0.pos_estimate
+        if abs(current_position - commanded_position) < position_delta_margin:
+            print(f"commanded_position: {commanded_position}")
+            break
+
+def setup(odrive, bounds_detection_routine_delta_time = 1):
+    odrive = bounds_detection_routine(
+            setup_velocity_control(
+                init(odrive)), bounds_detection_routine_delta_time)
+    setup_position_control(odrive)
+    return odrive
+
+__all__ = ['odrive', 'setup', 'move_in_procents']
---
+++ ODrive_Linear_Actuator_use_example.py
@@ -0,0 +1,22 @@
+from ODrive_Linear_Actuator import odrive, setup, move_in_procents
+from odrive.enums import AxisState
+import time
+
+odrives = [odrive.find_any()] # find all connected odrives and make an array of odrives
+
+odrv0 = setup(odrives[0], 1) # setup first odrive, with automatic bounds detection and position control
+
+time.sleep(2) # for testing purposes, wait for 2 second
+
+move_in_procents(odrv0, 12.5, True, 0.01) # move first odrive to 12.5% of its bounds,
+# wait for position to be reached set to True, with 0.01 margin
+# wait for position to be reached set to True makes the function wait until the position is reached,
+# and then the function returns, and the code continues to execute
+# with 0.01 margin makes the function wait until the position is reached with 0.01 margin
+
+# the fallowing code will execute after the position is reached
+# a time.sleep(X) can be added here to make sure the position is reached before
+# executing the fallowing code
+
+print(odrv0.axis0.pos_estimate) # odrive position right now in turns
+odrv0.axis0.requested_state = AxisState.IDLE # set odrive to idle state
